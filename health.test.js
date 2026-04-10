5b70a6c1c4d1f74577a7bfb3da98e43f8a917302
victosria <victoriascottimasiero@hotmail.com>
2026-04-09 21:27:25 -0300

    initial commit

---
+++ health.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const { createServer } = require('./server');
+
+const server = createServer();
+
+server.listen(0, async () => {
+  const port = server.address().port;
+  const url = `http://localhost:${port}/health`;
+
+  const res = await fetch(url);
+  const body = await res.json();
+
+  assert.strictEqual(res.status, 200);
+  assert.strictEqual(body.status, 'ok');
+
+  console.log('All tests passed!');
+  server.close();
+});
